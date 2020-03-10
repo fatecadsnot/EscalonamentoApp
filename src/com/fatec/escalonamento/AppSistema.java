f4ea106a3f5f7c4f57617cda80d527535861f92a
Iego Ribeiro <iego_aguiar@hotmail.com>
2020-03-09 21:10:24 -0300

    Looping AppSistema

--- src/com/fatec/escalonamento/AppSistema.java
+++ src/com/fatec/escalonamento/AppSistema.java
@@ -14,40 +14,52 @@ public class AppSistema {
 		Processos processo01 = new Processos(3);
 		Processos processo02 = new Processos(16);
 		Processos processo03 = new Processos(9);
-		Processos processo04 = new Processos(0);
+    Processos processo04 = new Processos(0);
+    
+    int escolha = 6;
+    
+    do {
+      System.out.println("Escolha o tipo de escalonamento");
+      System.out.println("1- FIFO");
+      System.out.println("2- SJF");
+      System.out.println("3- Algoritmos de prioridade");
+      System.out.println("4- Round Robbin");
+      System.out.println("5 - Finalizar programa");
+      
+      System.out.print("Digite uma opção: ");
+      escolha = scanner.nextInt();
+      
+      switch (escolha) {
+        case 1:
+          fifo.AdicionarProcesso(processo01);
+          fifo.AdicionarProcesso(processo02);
+          fifo.AdicionarProcesso(processo03);
+          fifo.calcularTempoMedio();
+          break;
+        case 2:
+          sjf.AdicionarProcesso(processo01);	
+          sjf.AdicionarProcesso(processo02);	
+          sjf.AdicionarProcesso(processo03);
+          sjf.AdicionarProcesso(processo04);	
+          sjf.calcularTempoMedio();
+          break;
+        case 3:
+          algPri.A();
+          break;
+        case 4:
+          
+          break;
+        
+        case 5:
+          System.out.println("Programa Finalizado");
+          break;
+      }
+
+      if (escolha < 1 || escolha > 5) {
+        System.out.println("Opção inválida!");
+        System.out.println("");
+      }
+    } while (escolha < 1 || escolha > 5); 
 		
-		System.out.println("Escolha o tipo de escalonamento");
-		System.out.println("1- FIFO");
-		System.out.println("2- SJF");
-		System.out.println("3- Algoritmos de prioridade");
-		System.out.println("4- Round Robbin");
-		int escolha = scanner.nextInt();
-		
-		switch (escolha) {
-		case 1:
-			fifo.AdicionarProcesso(processo01);
-			fifo.AdicionarProcesso(processo02);
-			fifo.AdicionarProcesso(processo03);
-			fifo.calcularTempoMedio();
-			break;
-		case 2:
-			sjf.AdicionarProcesso(processo01);	
-			sjf.AdicionarProcesso(processo02);	
-			sjf.AdicionarProcesso(processo03);
-			sjf.AdicionarProcesso(processo04);	
-			sjf.calcularTempoMedio();
-			break;
-		case 3:
-			algPri.A();
-			break;
-		case 4:
-			
-			break;
-		
-		default:
-			
-			break;
-		}
-	
 	}
 }
